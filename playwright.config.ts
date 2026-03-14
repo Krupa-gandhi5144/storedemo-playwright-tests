2cf766700d57db859b5a0102fea02a49414914eb
Gautam <gautam@Alphabins-iMac-Pro.local>
2026-03-14 16:14:13 +0530

    This is the 100 tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   snapshotDir: './__screenshots__', 
   fullyParallel: true,
   forbidOnly: isCI,
-  retries: isCI ? 1 : 1, // Enable retries for flaky test behavior
+  retries: isCI ? 3 : 3, // Enable retries for flaky test behavior
   workers: isCI ? 5 : 5,
 
   timeout: 60 * 1000,
